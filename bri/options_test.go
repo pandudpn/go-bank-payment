850b4ee3923f71bd3863a45ae16f3b78ebfd78c9
Pandu dwi Putra <pandu@pandudpn.id>
2022-04-20 20:42:17 +0700

    add unit test on bri.options

---
+++ bri/options_test.go
@@ -0,0 +1,27 @@
+package bri_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/pandudpn/go-bank-payment"
+	"github.com/pandudpn/go-bank-payment/bri"
+)
+
+func TestNewOption(t *testing.T) {
+	t.Run("testing options", func(t *testing.T) {
+		c := &bankpayment.APIImplementation{HTTPClient: &http.Client{}}
+		opts := bri.NewOption()
+		opts.SetConsumerSecret("abc")
+		opts.SetAccessToken("abc")
+		opts.SetDevelopment()
+		opts.SetProduction()
+		opts.SetConsumerKey("abc")
+		opts.GetConsumerSecret()
+		opts.GetConsumerKey()
+		opts.GetAccessToken()
+		opts.GetEnvironment()
+		opts.SetAPIRequest(c)
+		opts.SetHTTPClient(&http.Client{})
+	})
+}
